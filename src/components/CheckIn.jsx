27d3f7265c7c5517a02e4cd6300a2314aafdf757
G808 <hucciallstars@gmail.com>
2023-01-15 20:59:15 +0200

    Added react date picker

--- src/components/CheckIn.jsx
+++ src/components/CheckIn.jsx
@@ -1,7 +1,23 @@
-import React from 'react';
+import React, { useState } from "react";
+//react date picker
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import "../../datepicker.css";
+
+import { BsCalendar } from "react-icons/bs";
 
 const CheckIn = () => {
-  return <div>CheckIn</div>;
+  const [startDate, setStartDate] = useState(false);
+  return (
+    <div>
+      <DatePicker
+        className="h-full w-full"
+        selected={startDate}
+        placeholderText="Check In"
+        onChange={(date) => setStartDate(date)}
+      />
+    </div>
+  );
 };
 
 export default CheckIn;
