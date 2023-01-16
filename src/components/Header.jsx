750070d75873667510d509daac852b0fd985ad6e
G808 <hucciallstars@gmail.com>
2023-01-16 22:00:08 +0200

    Fixed Header logo gap

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -14,16 +14,16 @@ const Header = () => {
   return (
     <header
       className={`${
-        header ? "bg-white py-6 shadow-lg" : "bg-transparent py-8"
+        header ? "bg-white py-4 shadow-lg" : "bg-transparent py-4"
       } fixed z-50 w-full transition-all duration-500`}
     >
-      <div className="container mx-auto flex flex-col items-center gap-y-6 lg:flex-row lg:justify-between lg:gap-y-0">
+      <div className="container mx-auto flex flex-col items-center gap-y-2 lg:flex-row lg:justify-between lg:gap-y-0">
         {/* logo */}
         <a href="/">
           {header ? (
-            <img className="h-[75px] w-[125px]" src={logoDark} />
+            <img className="h-[55px] w-[125px]" src={logoDark} />
           ) : (
-            <img className="h-[75px] w-[125px]" src={logoLight} />
+            <img className="h-[55px] w-[125px]" src={logoLight} />
           )}
         </a>
         {/* nav items */}
