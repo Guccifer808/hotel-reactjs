5c4ad5cb95e47d6826f6d087dc8824d5e73940d0
G808 <hucciallstars@gmail.com>
2023-01-16 21:28:36 +0200

    Added loader/spinner

--- src/components/Rooms.jsx
+++ src/components/Rooms.jsx
@@ -2,11 +2,18 @@ import React, { useContext } from "react";
 import Room from "../components/Room";
 //context
 import { RoomContext } from "../context/RoomContext";
-
+import { SpinnerDiamond } from "spinners-react";
 const Rooms = () => {
-  const { rooms } = useContext(RoomContext);
+  const { rooms, loading } = useContext(RoomContext);
+
   return (
     <section className="py-24">
+      {/* loader, overlay */}
+      {loading && (
+        <div className="fixed bottom-0 top-0 z-50 flex h-screen w-full items-center justify-center bg-black/70">
+          <SpinnerDiamond color="#a37d4c" />
+        </div>
+      )}
       <div className="container mx-auto lg:px-0">
         {/* grid */}
         <div className="mx-auto grid max-w-sm grid-cols-1 gap-7 lg:mx-0 lg:max-w-none lg:grid-cols-3">
--- src/context/RoomContext.jsx
+++ src/context/RoomContext.jsx
@@ -11,24 +11,31 @@ const RoomProvider = ({ children }) => {
   const [adults, setAdults] = useState("1 Adult");
   const [kids, setKids] = useState("No Kids");
   const [total, setTotal] = useState(0);
+  const [loading, setLoading] = useState(false);
   //total
   useEffect(() => {
     setTotal(Number(adults[0]) + Number(kids[0]));
   });
-  //
+  //onClick handler
   const handleClick = (e) => {
     e.preventDefault();
+    //loader
+    setLoading(true);
     //filter rooms depending on total person count
     const newRooms = roomData.filter((room) => {
       return total <= room.maxPerson;
     });
-    setRooms(newRooms);
+    //loader
+    setTimeout(() => {
+      setRooms(newRooms);
+      setLoading(false);
+    }, 1500);
   };
   // console.log(rooms);
 
   return (
     <RoomContext.Provider
-      value={{ rooms, adults, setAdults, kids, setKids, handleClick }}
+      value={{ rooms, adults, setAdults, kids, setKids, handleClick, loading }}
     >
       {children}
     </RoomContext.Provider>
